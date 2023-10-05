b7490253f06ad46ac46567b5b1d17e3de7562aef
Mert <79603812+ufcovo@users.noreply.github.com>
2023-10-05 23:24:11 +0300

    Task ( async-await)

---
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+            // some business code without non async
+            var data = await myTask;
+            return Ok(data);
+        }
+    }
+}
