cbf8b2a79a3d088b0273532ef98d45c47510d5ef
Mert <79603812+ufcovo@users.noreply.github.com>
2023-10-05 23:53:20 +0300

    Task Method: ContinueWith

---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// First Way
+// GetStringAsync methodundan google com geldikten sonra ContinueWith çalışacak.
+var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(data =>
+{
+    Console.WriteLine("Data Length: " + data.Result.Length);
+});
+Console.WriteLine("Arada yapılacak işler.");
+await myTask;
+
+
+// Second way (Best practice)
+var myTask2 = new HttpClient().GetStringAsync("https://www.google.com");
+Console.WriteLine("Arada yapılacak işler.2");
+var data = await myTask2;
+Console.WriteLine("Data Length2: " + data.Length);
