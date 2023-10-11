a64123109b101603cb85adc60a31c5010a3ea171
Mert <79603812+ufcovo@users.noreply.github.com>
2023-10-11 21:36:23 +0300

    WhenAll method

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -1,18 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-// First Way
-// GetStringAsync methodundan google com geldikten sonra ContinueWith çalışacak.
-var myTask = new HttpClient().GetStringAsync("https://www.google.com").ContinueWith(data =>
+﻿namespace TaskConsoleApp
 {
-    Console.WriteLine("Data Length: " + data.Result.Length);
-});
-Console.WriteLine("Arada yapılacak işler.");
-await myTask;
+    public class Content
+    {
+        public string Site { get; set; }
+        public int Length { get; set; }
+    };
 
 
-// Second way (Best practice)
-var myTask2 = new HttpClient().GetStringAsync("https://www.google.com");
-Console.WriteLine("Arada yapılacak işler.2");
-var data = await myTask2;
-Console.WriteLine("Data Length2: " + data.Length);
+    internal class Program
+    {
+        private async static Task Main(string[] ars)
+        {
+            await Console.Out.WriteLineAsync("Main thread:" + Thread.CurrentThread.ManagedThreadId);
+            List<string> urlsList = new List<string>()
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.amazon.com",
+                "https://www.n11.com",
+                "https://www.haberturk.com"
+            };
+            List<Task<Content>> taskList = new List<Task<Content>>();
+            urlsList.ToList().ForEach(url =>
+            {
+                taskList.Add(GetContentAsync(url));
+            });
+            var contents = Task.WhenAll(taskList.ToArray());
+            await Console.Out.WriteLineAsync("WhenAll sonrası gerçekleşen  methodlar.");
+            var data = await contents;
+            data.ToList().ForEach(task =>
+            {
+                Console.WriteLine($"{task.Site} Length: {task.Length}");
+            });
+
+        }
+        public static async Task<Content> GetContentAsync(string url)
+        {
+            Content content = new Content();
+            var data = await new HttpClient().GetStringAsync(url);
+            content.Site = url;
+            content.Length = data.Length;
+            await Console.Out.WriteLineAsync("GetContentAsync thread:" + Thread.CurrentThread.ManagedThreadId);
+            return content;
+        }
+    }
+}
