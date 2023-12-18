7b889905fee1f73e64f215c248322a92df995e72
Mert <79603812+ufcovo@users.noreply.github.com>
2023-12-18 21:06:20 +0300

    StartNew()

--- TaskConsoleApp/Program.cs
+++ TaskConsoleApp/Program.cs
@@ -5,44 +5,26 @@
         public string Site { get; set; }
         public int Length { get; set; }
     };
-
+    public class Status
+    {
+        public int ThreadId { get; set; }
+        public DateTime Date { get; set; }
+    };
 
     internal class Program
     {
         private async static Task Main(string[] ars)
         {
-            await Console.Out.WriteLineAsync("Main thread:" + Thread.CurrentThread.ManagedThreadId);
-            List<string> urlsList = new List<string>()
+            var myTask = Task.Factory.StartNew((obj) =>
             {
-                "https://www.google.com",
-                "https://www.microsoft.com",
-                "https://www.amazon.com",
-                "https://www.n11.com",
-                "https://www.haberturk.com"
-            };
-            List<Task<Content>> taskList = new List<Task<Content>>();
-            urlsList.ToList().ForEach(url =>
-            {
-                taskList.Add(GetContentAsync(url));
-            });
-
-            var contents = await Task.WhenAll(taskList.ToArray());
-            contents.ToList().ForEach(task =>
-            {
-                Console.WriteLine(task.Site);
-            });
-        }
-        public static async Task<Content> GetContentAsync(string url)
-        {
-            Content content = new Content();
-            var data = await new HttpClient().GetStringAsync(url);
-
-            await Task.Delay(5000);
-
-            content.Site = url;
-            content.Length = data.Length;
-            await Console.Out.WriteLineAsync("GetContentAsync thread:" + Thread.CurrentThread.ManagedThreadId);
-            return content;
+                Console.WriteLine("MyTask start");
+                var status = obj as Status;
+                status.ThreadId = Thread.CurrentThread.ManagedThreadId;
+            }, new Status() { Date = DateTime.Now});
+            await myTask;
+            Status s = myTask.AsyncState as Status;
+            Console.WriteLine(s.Date);
+            Console.WriteLine(s.ThreadId);
         }
     }
 }
